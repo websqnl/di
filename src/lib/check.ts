b32ba8fbebd08665b48ab8be39fb342d753338ab
Gui Seek <guiseek@gmail.com>
2024-08-06 23:56:34 -0300

    feat: async providers

--- src/lib/check.ts
+++ src/lib/check.ts
@@ -4,6 +4,9 @@ export const check = {
   isFn<T>(value: unknown): value is Fn<T> {
     return typeof value === 'function'
   },
+  isAsyncFn<T>(value: unknown): value is Fn<Promise<T>> {
+    return this.isFn(value) && value.constructor.name === 'AsyncFunction'
+  },
   isType<T>(value: unknown): value is Type<T> {
     return this.isFn(value) && typeof value.prototype === 'object'
   },
--- src/lib/main.ts
+++ src/lib/main.ts
@@ -12,7 +12,7 @@ function use<T>(ref: Ref<T>): T {
   return value
 }
 
-function provide<T>({ref, use}: Provider<T>) {
+const provide = async <T>({ref, use}: Provider<T>) => {
   const type = (use ?? ref) as T
 
   if (check.isFn<T>(type)) {
@@ -22,22 +22,32 @@ function provide<T>({ref, use}: Provider<T>) {
       return new type(...deps)
     }
 
+    if (check.isAsyncFn<T>(type)) {
+      return await type(...deps)
+    }
+
     return type(...deps)
   }
 
   return type
 }
 
-function add<T>(provider: Provider<T>) {
+const add = async <T>(provider: Provider<T>) => {
   if (provider.dep && provider.dep.length > 0) {
     relations.set(provider.ref, provider.dep.map(use))
   }
 
-  container.set(provider.ref, provide(provider))
+  container.set(provider.ref, await provide(provider))
+
+  return use(provider.ref)
 }
 
-function set(...providers: Provider<any>[]) {
-  providers.map(add)
+async function* set<T>(
+  ...providers: Provider<T>[]
+): AsyncGenerator<T, void, unknown> {
+  for (const p of providers) {
+    yield await add(p)
+  }
 }
 
 export {use, add, set}
